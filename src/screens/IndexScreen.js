51e7ea010fc40b5e592f4657f5333104216a8ac7
Juliana Leon <juleon80@gmail.com>
2021-03-21 20:25:19 +0000

    my first commit

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default IndexScreen;
